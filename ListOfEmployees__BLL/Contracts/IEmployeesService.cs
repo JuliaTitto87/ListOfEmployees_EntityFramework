66b515013b1871777224282809386b5436265d38
JuliaTitto87 <133301250+JuliaTitto87@users.noreply.github.com>
2024-03-24 13:24:20 +0300

    Created nrew migration

---
+++ ListOfEmployees__BLL/Contracts/IEmployeesService.cs
@@ -0,0 +1,18 @@
+﻿using ListOfEmployees__DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfEmployees__BLL.Contracts
+{
+    public interface IEmployeesService
+    {
+        List<Employee> GetEmployees();
+        void DeleteEmployee(Employee employee);
+        void AddEmployee(Employee employee);
+
+        void UpdateEmployee(Employee employee);
+    }
+}
